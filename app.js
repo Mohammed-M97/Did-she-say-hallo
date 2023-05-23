03c3d6ef9478e0bfadfdfc21ccdf7077c4db19ce
Mohammed Al-Awlaqi <mohammed-alawlaqi@outlook.com>
2023-05-23 22:30:15 +0300

    add a new line

---
+++ app.js
@@ -0,0 +1,6 @@
+function validateHello(greetings) {
+  res = /hello|ciao|salut|hallo|hola|ahoj|czesc/i.test(greetings) //regex here/.test(greetings) 
+  return res
+}
+
+console.log(validateHello('hello'));
